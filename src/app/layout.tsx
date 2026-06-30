959f984fcaff38a6fc8da889e28f79c5622d6b02
mrdrobotE <yeshimbet06@gmail.com>
2026-06-30 07:12:06 -0900

    Remove ts-expect-error comment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { ThemeProvider } from 'next-themes';
 import { Onest } from 'next/font/google';
 // src/app/layout.tsx
-// @ts-expect-error - CSS import
 import './globals.css';
 import { ToasterProvider } from './providers/toaster';
 
